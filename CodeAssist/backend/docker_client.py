33b4c2694933c08c0c6ae59456b5143c3caa738c
Ricky Woodruff <woodruffr4@gmail.com>
2022-09-06 13:25:09 -0500

    auto remove docker containers on exit

--- CodeAssist/backend/docker_client.py
+++ CodeAssist/backend/docker_client.py
@@ -2,19 +2,26 @@ import docker
 import os
 import shutil
 
-client = docker.from_env()
 docker_dir = '/Users/rickywoodruff/Desktop/UT Austin/Fall 2022 (Senior)/CS370/codeAssist/CodeAssist/backend/dockerfiles'
 runs_dir = '/Users/rickywoodruff/Desktop/UT Austin/Fall 2022 (Senior)/CS370/codeAssist/CodeAssist/backend/runs'
+client = docker.from_env()
 
 def run_container(container: str, file, filename, uuid):
     run_dir = os.path.join(runs_dir, uuid)
     setup_directory(container, file, filename, run_dir)
-    image = client.images.build(path=run_dir, tag=container)
-    res = client.containers.run(image=image[0])
+
+    # Create docker image
+    image = client.images.build(path=run_dir, tag=container, rm=True)[0]
+    # Execute docker container
+    res = client.containers.run(image=image, auto_remove=True)
+
     clean_directory(run_dir)
     return res
 
 def setup_directory(container, file, filename, run_dir):
+    if not os.path.exists(runs_dir):
+        os.mkdir(runs_dir)
+        
     os.mkdir(run_dir)
     file.save(os.path.join(run_dir, filename))
     shutil.copy(os.path.join(docker_dir, container, "Dockerfile"), run_dir)
