ad1e2ce8a62ce4e096c53439365708643196e8d6
Ricky Woodruff <woodruffr4@gmail.com>
2022-09-15 17:05:33 -0500

    add dropdown and styling

--- CodeAssist/frontend/src/components/FileUpload.js
+++ CodeAssist/frontend/src/components/FileUpload.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios"
 
-const FileUpload = (props) => {
+const FileUpload = () => {
 
     const [file, setFile] = useState(null);
     const [response, setResponse] = useState(null);
@@ -19,10 +19,19 @@ const FileUpload = (props) => {
 
     return (
         <div>
-            <div>
+            <div id="form">
                 <form onSubmit={uploadFile} encType="multipart/form-data">
-                    <input type="file" name="file" onChange={e => setFile(e.target.files[0])}/>
-                    <input type="submit"/>
+                    <div id="dockerfile">
+                        <p className="inline">Select assignment</p>
+                        <select className="inline">
+                            <option value="test">test</option>
+                        </select>
+                    </div>
+                    <div id="file">
+                        <p className="inline">Choose file</p>
+                        <input id="fileUpload" type="file" name="file" onChange={e => setFile(e.target.files[0])}/>
+                        <input id="submit" type="submit"/>
+                    </div>
                 </form>
             </div>
             <div>
