6f3c981b81c4cd552e775ee715cc9fa75620d10d
jamisen-ma <100271213+jamisen-ma@users.noreply.github.com>
2023-02-10 13:45:19 -0600

    Can see assignments in dashboard now.

--- frontend/src/pages/instructor/dashboard/index.js
+++ frontend/src/pages/instructor/dashboard/index.js
@@ -9,18 +9,20 @@ import {
   Space,
   Table,
 } from "antd";
-import { tableData, texts } from "./constant";
+import { texts } from "./constant";
 import TextItem from "./TextItem";
 import moment from "moment";
 import { Link, useParams } from "react-router-dom";
 import { useContext } from "react";
 import { GlobalContext } from "../../../App";
-import { useEffect } from "react";
-
+import { useEffect, useState } from "react";
+const api_url = "http://localhost:5000/get_course_assignments"
+const course_id= "577031b8-ac04-4c33-84bf-32a82c2ce5ba"  //need to figure out how to find global course_id, 
+//for now change this to a course_id you have in the database
 const columns = [
   {
     title: "ACTIVE ASSIGNMENTS",
-    dataIndex: "assignmentName",
+    dataIndex: "name",
     sorter: (a, b) => a.assignmentName > b.assignmentName,
     render: (text, record) => (
       <Link to={`/assignment/reviewGrades/${record.id}`}>{text}</Link>
@@ -66,9 +68,94 @@ const columns = [
   },
 ];
 
+
 export default function InstructorDashboard() {
   const { courseId } = useParams();
+  const [data, setData] = useState(null);
   const { courseInfo, updateCourseInfo } = useContext(GlobalContext);
+  
+  
+  
+  
+//   async function get(url, params){
+//     let response = await fetch(url + '?' + new URLSearchParams(params))
+//     let data = await response.json()
+//     data = JSON.stringify(data)
+//     return JSON.parse(data)
+// }
+  
+//   function main() {
+//     //OPTION 1
+
+
+//     //OPTION 2
+//     let jsondata = get('http://localhost:5000/get_course_assignments', {
+//       course_id: "577031b8-ac04-4c33-84bf-32a82c2ce5ba",
+//     })
+  
+//     return Promise.resolve(jsondata)
+//   }
+//   // let assignments1 = main();
+//   // console.log(assignments1);
+
+//   var p = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
+//       resolve('foo');
+//     }, 300);
+//   });
+  
+//   console.log(p);
+
+
+
+
+  // function getdata(url, params) {
+  //   return fetch(url + '?' + new URLSearchParams(params))
+  //     .then(function(response) {
+  //       return response.json();
+  //     })
+  //     .then(function(data) {
+  //       console.log(data);
+  //       //var userid = JSON.parse(data);
+  //       //console.log(userid);
+   
+  //       return data;
+  //     })
+  // }
+
+  // async function fetchData(url, params){
+  //   let response = await fetch(url + '?' + new URLSearchParams(params));
+  //   let data = await response.json();
+  //   data = JSON.stringify(data);
+  //   data = JSON.parse(data);
+  //   return data;
+  //  }
+  
+
+   const getData = async (url, params) => {
+
+    fetch(url + '?' + new URLSearchParams(params))
+       .then((response) => {
+           if (!response.ok) {
+               console.log("ok")
+           }
+           return response.json();
+       })
+       .then(json => setData(json))
+       .catch((error) => {
+
+       })
+  }
+
+  useEffect(() => {
+    getData(api_url, {
+            course_id: course_id,
+          });
+},[])
+
+
+
+  console.log(data)
 
   useEffect(() => {
     if (!courseInfo.id) {
@@ -87,7 +174,7 @@ export default function InstructorDashboard() {
           <span>Course ID: 394120</span>
         </Space>
       </div> */}
-      <PageHeader title='C S N313E' subTitle='Summer 2022'>
+      <PageHeader title='CS 313E' subTitle='Summer 2022'>
         <Descriptions>
           <Descriptions.Item label='Course ID'>394120</Descriptions.Item>
         </Descriptions>
@@ -118,7 +205,7 @@ export default function InstructorDashboard() {
         </Space>
       </Card>
       <Card bordered={false} bodyStyle={{ paddingTop: 0 }}>
-        <Table rowKey='id' columns={columns} dataSource={tableData} />
+        <Table rowKey='id' columns={columns} dataSource={data} />
       </Card>
     </>
   );
