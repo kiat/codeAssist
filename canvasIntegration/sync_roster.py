dcdc2bca46ca23e6fbd48a7b0f8cd96e1e969b5c
jamisen-ma <100271213+jamisen-ma@users.noreply.github.com>
2023-03-31 15:55:11 -0500

    Update sync_roster.py
    Add comments

--- canvasIntegration/sync_roster.py
+++ canvasIntegration/sync_roster.py
@@ -3,8 +3,22 @@ import psycopg2
 import uuid
 import requests
 from canvasMethods import *
+"""
+This script synchronizes course and roster information between Canvas and the CodeAssist database.
+It connects to the local PostgreSQL database and retrieves course, instructor, assignment, and student
+information from Canvas using the Canvas API. It then checks if the retrieved data already exists in the
+database, and if not, adds the new data.
+"""
 
 def sync_roster_and_courses(cursor, connection):
+    """
+        Synchronize the roster and courses information between Canvas and the local database.
+
+        Args:
+            cursor (psycopg2.extensions.cursor): The database cursor.
+            connection (psycopg2.extensions.connection): The database connection.
+    """
+
     info_dict = get_data_from_canvas()
     courses = info_dict["courses"]
     assignments = info_dict["assignments"]
@@ -26,7 +40,7 @@ def sync_roster_and_courses(cursor, connection):
                 new_instructor_id = put_instructor_in_database(cursor, instructor_name, instructor_email,
                                                                sis_instructor_id)
                 put_course_in_database(cursor, course_name, new_instructor_id, course_id, sis_instructor_id)
-    print("fag", assignments)
+ 
     connection.commit()
     for assignment in assignments:
         sis_assignment_id = assignment["sis_assignment_id"]
@@ -36,15 +50,14 @@ def sync_roster_and_courses(cursor, connection):
         points = assignment["points"]
         course_uuid = check_if_course_in_database(cursor, sis_course_id)
         assignment_bool = check_if_assignment_in_database(cursor, sis_assignment_id)
-        print("eerererer")
+        
         if assignment_bool:
             print("Assignment already in database")
         else:
-            print("hello " + assignment_name)
             assignment_id = put_assignment_in_database(cursor, assignment_name, course_uuid, points, "2011-09-20",
                                                        sis_assignment_id, sis_course_id)
             print("Added new assignment: " + assignment_name)
-    print("fat")
+
 
     students = info_dict["students"]
     for student in students:
@@ -62,6 +75,9 @@ def sync_roster_and_courses(cursor, connection):
 
 
 def connect_to_database():
+    """
+    Connect to the CodeAssist database, synchronize the roster and courses information, and close the connection.
+    """
     try:
         connection = psycopg2.connect(user="postgres",
                                       password="Asiangoat343623",
