e064e9b762e2bb9017b8a24bebf2edc1ef599017
Kyildirim24 <kamilyildirim24@gmail.com>
2023-06-20 20:35:27 -0500

    fixed problem with reloading instructor dashboard

--- frontend/src/pages/instructor/dashboard/index.js
+++ frontend/src/pages/instructor/dashboard/index.js
@@ -16,8 +16,8 @@ import { Link, useParams } from "react-router-dom";
 import { useContext } from "react";
 import { GlobalContext } from "../../../App";
 import { useEffect, useState } from "react";
-const api_url = "http://localhost:5000/get_course_assignments"
-const course_id= "577031b8-ac04-4c33-84bf-32a82c2ce5ba"  //need to figure out how to find global course_id, 
+const api_url = "http://localhost:5000/get_course_assignments";
+const course_id = "577031b8-ac04-4c33-84bf-32a82c2ce5ba"; //need to figure out how to find global course_id,
 //for now change this to a course_id you have in the database
 const columns = [
   {
@@ -32,13 +32,13 @@ const columns = [
     title: "RELEASED",
     dataIndex: "released",
     sorter: (a, b) => a.released - b.released,
-    render: text => moment(text).format("MMM DD [AT] h:mmA").toUpperCase(),
+    render: (text) => moment(text).format("MMM DD [AT] h:mmA").toUpperCase(),
   },
   {
     title: "DUE(CDT)",
     dataIndex: "due",
     sorter: (a, b) => a.due - b.due,
-    render: text => moment(text).format("MMM DD [AT] h:mmA").toUpperCase(),
+    render: (text) => moment(text).format("MMM DD [AT] h:mmA").toUpperCase(),
   },
   {
     title: "SUBMISSIONS",
@@ -49,14 +49,14 @@ const columns = [
     title: "% GRADED",
     dataIndex: "graded",
     sorter: (a, b) => a.graded - b.graded,
-    render: text => <Progress percent={text} size='small' status='normal' />,
+    render: (text) => <Progress percent={text} size="small" status="normal" />,
   },
   {
     title: "PUBLISHED",
     dataIndex: "published",
     sorter: (a, b) => a.published - b.published,
-    render: text => (
-      <Button type={text ? "primary" : "default"} shape='circle' size='small'>
+    render: (text) => (
+      <Button type={text ? "primary" : "default"} shape="circle" size="small">
         {" "}
       </Button>
     ),
@@ -68,46 +68,38 @@ const columns = [
   },
 ];
 
-
 export default function InstructorDashboard() {
   const { courseId } = useParams();
   const [data, setData] = useState(null);
   const { courseInfo, updateCourseInfo } = useContext(GlobalContext);
-  
-  
-  
-  
-//   async function get(url, params){
-//     let response = await fetch(url + '?' + new URLSearchParams(params))
-//     let data = await response.json()
-//     data = JSON.stringify(data)
-//     return JSON.parse(data)
-// }
-  
-//   function main() {
-//     //OPTION 1
+  const user_id = useContext(GlobalContext).userInfo.id;
+  //   async function get(url, params){
+  //     let response = await fetch(url + '?' + new URLSearchParams(params))
+  //     let data = await response.json()
+  //     data = JSON.stringify(data)
+  //     return JSON.parse(data)
+  // }
 
+  //   function main() {
+  //     //OPTION 1
 
-//     //OPTION 2
-//     let jsondata = get('http://localhost:5000/get_course_assignments', {
-//       course_id: "577031b8-ac04-4c33-84bf-32a82c2ce5ba",
-//     })
-  
-//     return Promise.resolve(jsondata)
-//   }
-//   // let assignments1 = main();
-//   // console.log(assignments1);
+  //     //OPTION 2
+  //     let jsondata = get('http://localhost:5000/get_course_assignments', {
+  //       course_id: "577031b8-ac04-4c33-84bf-32a82c2ce5ba",
+  //     })
 
-//   var p = new Promise(function(resolve, reject) {
-//     setTimeout(function() {
-//       resolve('foo');
-//     }, 300);
-//   });
-  
-//   console.log(p);
+  //     return Promise.resolve(jsondata)
+  //   }
+  //   // let assignments1 = main();
+  //   // console.log(assignments1);
 
+  //   var p = new Promise(function(resolve, reject) {
+  //     setTimeout(function() {
+  //       resolve('foo');
+  //     }, 300);
+  //   });
 
-
+  //   console.log(p);
 
   // function getdata(url, params) {
   //   return fetch(url + '?' + new URLSearchParams(params))
@@ -118,7 +110,7 @@ export default function InstructorDashboard() {
   //       console.log(data);
   //       //var userid = JSON.parse(data);
   //       //console.log(userid);
-   
+
   //       return data;
   //     })
   // }
@@ -130,38 +122,50 @@ export default function InstructorDashboard() {
   //   data = JSON.parse(data);
   //   return data;
   //  }
-  
 
-   const getData = async (url, params) => {
-
-    fetch(url + '?' + new URLSearchParams(params))
-       .then((response) => {
-           if (!response.ok) {
-               console.log("ok")
-           }
-           return response.json();
-       })
-       .then(json => setData(json))
-       .catch((error) => {
-
-       })
-  }
+  const getData = async (url, params) => {
+    fetch(url + "?" + new URLSearchParams(params))
+      .then((response) => {
+        if (!response.ok) {
+          console.log("ok");
+        }
+        return response.json();
+      })
+      .then((json) => setData(json))
+      .catch((error) => {});
+  };
 
   useEffect(() => {
     getData(api_url, {
-            course_id: course_id,
-          });
-},[])
-
-
-
-  console.log(data)
+      course_id: courseId,
+    });
+  }, []);
 
   useEffect(() => {
     if (!courseInfo.id) {
       updateCourseInfo({
         id: courseId,
+        name: courseInfo.name,
       });
+    }
+    if (!courseInfo.name) {
+      fetch(
+        "http://localhost:5000/get_instructor_courses?" +
+          new URLSearchParams({
+            instructor_id: user_id,
+          })
+      )
+        .then((res) => res.json())
+        .then((data) =>
+          data.forEach((element) => {
+            if (element.id === courseId) {
+              updateCourseInfo({
+                id: courseId,
+                name: element.name,
+              });
+            }
+          })
+        );
     }
   });
 
@@ -174,9 +178,9 @@ export default function InstructorDashboard() {
           <span>Course ID: 394120</span>
         </Space>
       </div> */}
-      <PageHeader title={courseInfo.name} subTitle='Summer 2022'>
+      <PageHeader title={courseInfo.name} subTitle="Summer 2022">
         <Descriptions>
-          <Descriptions.Item label='Course ID'>394120</Descriptions.Item>
+          <Descriptions.Item label="Course ID">{courseId}</Descriptions.Item>
         </Descriptions>
       </PageHeader>
       <Card bordered={false} bodyStyle={{ paddingTop: 0 }}>
@@ -192,7 +196,7 @@ export default function InstructorDashboard() {
       <Card bordered={false} bodyStyle={{ paddingTop: 0 }}>
         <h3>THINGS TO DO</h3>
         <Divider style={{ marginTop: 0, marginBottom: "5px" }} />
-        <Space direction='vertical'>
+        <Space direction="vertical">
           {texts.map((item, index) => (
             <TextItem text={item} key={index} />
           ))}
@@ -205,7 +209,7 @@ export default function InstructorDashboard() {
         </Space>
       </Card>
       <Card bordered={false} bodyStyle={{ paddingTop: 0 }}>
-        <Table rowKey='id' columns={columns} dataSource={data} />
+        <Table rowKey="id" columns={columns} dataSource={data} />
       </Card>
     </>
   );
