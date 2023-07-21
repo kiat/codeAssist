bf12129c5a93c49f77109a25f233f4c38b20477d
Allen Wu <allenwuhoo@gmail.com>
2023-07-20 21:25:13 -0700

    better assignment modal interface

--- frontend/src/pages/assignments/assignment_modal.js
+++ frontend/src/pages/assignments/assignment_modal.js
@@ -1,37 +1,61 @@
-import React, { useState } from 'react';
-import { message, Modal, Upload } from "antd";
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, message, Form, Modal, Upload } from "antd";
+import { InboxOutlined } from '@ant-design/icons';
 
 export default function AssignmentModal({ open, onCancel, submit }) {
 
-    const [loading, setLoading] = useState(false);
-
-    // TODO: ALTER THIS FOR ALL RELEVANT FILE RESTRICTIONS.
-    const beforeUpload = (file) => {
-        const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-        if (!isJpgOrPng) {
-          message.error('You can only upload JPG/PNG file!');
+    const { Dragger } = Upload;
+    const props = {
+      name: 'file',
+      multiple: true,
+      // action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+      onChange(info) {
+        const { status } = info.file;
+        if (status !== 'uploading') {
+          console.log(info.file, info.fileList);
         }
-        const isLt2M = file.size / 1024 / 1024 < 2;
-        if (!isLt2M) {
-          message.error('Image must smaller than 2MB!');
+        if (status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
         }
-        return isJpgOrPng && isLt2M;
-      };
+      },
+      onDrop(e) {
+        console.log('Dropped files', e.dataTransfer.files);
+      },
+    };
 
-    const uploadButton = (
-        <div>
-          {loading ? <LoadingOutlined /> : <PlusOutlined />}
-          <div style={{ marginTop: 8 }}>Upload</div>
-        </div>
-      );
+    const getFile = (e) => {
+      console.log('Upload event: ', e);
+      if (Array.isArray(e)) {
+        return e;
+      }
+     return e && e.fileList;
+    }
+
+    const submitAssignment = async (fileList) => {
+      console.log(fileList)
+    }
 
     return (
-        <Modal title="Submit Assignment" open={open} onCancel={onCancel}>
-            <Upload name="submission" listType="picture-card" className="submission-uploader" showUploadList={false} >
-                {/* YOU MUST SET UPLOAD ACTION HERE IN UPLOAD. THIS MUST CONNECT TO THE DATABASE*/}
-                {uploadButton}
-            </Upload>
+        <Modal title="Submit Assignment" open={open} onCancel={onCancel} footer={null}>
+          <Form layout="vertical" onFinish={submitAssignment}>
+            <Form.Item name="upload" getValueFromEvent={getFile}>
+              <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                <p className="ant-upload-hint">
+                  Support for a single or bulk upload.
+                </p>
+              </Dragger>
+            </Form.Item>
+            <Form.Item>
+              <Button style={{ width: "100%" }} type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </Modal>
     );
 }
