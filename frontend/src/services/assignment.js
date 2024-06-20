61da7a92ee9c879cba80c1cfcf6818e9b8610ad4
songbrandon7 <songbrandon7@gmail.com>
2024-06-20 15:50:03 -0500

    updated services.js to include delete method, used service method in deleting assignments, and added 2 functions for centralized api calling

--- frontend/src/services/assignment.js
+++ frontend/src/services/assignment.js
@@ -1,9 +1,17 @@
 import service from ".";
 
+export async function createAssignment(params) {
+  return service("create_assginment", params, "post");
+}
+
 export async function getAssignment(params) {
   return service("get_assignment", params);
 }
 
 export async function updateAssignment(params) {
   return service("update_assignment", params, "post");
+}
+
+export async function deleteAssignment(params) {
+  return service("delete_assignment", params, "delete");
 }
