5fb106403e2bd819bd9210743be31a692c3197d5
Makyra Kim <makyra@wireless-10-155-176-236.public.utexas.edu>
2025-11-07 13:06:28 -0600

    fixed addForm test issue

--- frontend/src/pages/dashboard/addForm.js
+++ frontend/src/pages/dashboard/addForm.js
@@ -2,15 +2,14 @@ import { Button, Form, Input, Select } from "antd";
 // import { formItemList } from "./constant";
 
 export default function AddForm({ onFinish, onCancel }) {
-
+  const handleFinish = (values) => {
+    // dropping unused/hidden field if present
+    const { allowEntryCode, ...payload } = values;
+    onFinish(payload);
+  };
 
   return (
-    <Form layout='vertical' onFinish={onFinish} initialValues={{ allowEntryCode: true }}>
-      {/* {formItemList.map(item => (
-        <Form.Item label={item.label} name={item.name} key={item.name}>
-          <Input />
-        </Form.Item>
-      ))} */}
+    <Form layout="vertical" onFinish={handleFinish} initialValues={{ allowEntryCode: true }}>
       <Form.Item label='COURSE NAME' name='courseName'>
         <Input />
       </Form.Item>
@@ -32,6 +31,8 @@ export default function AddForm({ onFinish, onCancel }) {
       </Form.Item>
 
       {/* ensure allowEntryCode is included even without a visible field */}
+      {/* while the allowEntryCode option (allowing students to enroll themselves to course) is not enabled, leaving it for 
+          potential future usage */}
       <Form.Item name='allowEntryCode' initialValue={true} hidden>
         <Input type='hidden' />
       </Form.Item>
