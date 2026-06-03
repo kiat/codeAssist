86dd26a5649bb6cfc9b4c6d223fa7e6847e61972
cecilia zhang <2022cicizhang@gmail.com>
2026-06-03 17:34:19 -0500

    frontend saving issue resolved

--- frontend/src/services/course.js
+++ frontend/src/services/course.js
@@ -51,3 +51,7 @@ export async function updateCourse(params) {
 export async function updateRole(params) {
   return service("update_role", params, "post");
 }
+
+export async function storeApiKey(params) {
+  return service("store_api_key", params, "put");
+}
